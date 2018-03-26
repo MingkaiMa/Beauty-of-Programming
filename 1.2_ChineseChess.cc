c76d4ec52c622ad9064cfcd2ed4326b2c91c9246
Mingkai Ma <mamingkai824@gmail.com>
2018-03-27 02:00:47 +1100

    Chinese Chess bit

--- 1.2_ChineseChess.cc
+++ 1.2_ChineseChess.cc
@@ -13,11 +13,29 @@
 
 int main()
 {
-    unsigned char b = '\0';
-    for(LSET(b, 1); LGET(b) <= GRIDW * GRIDW; LSET(b, (LGET(b) + 1)))
-        for(RSET(b, 1); RGET(b) <= GRIDW * GRIDW; RSET(b, (RGET(b) + 1)))
-            if(LGET(b) % GRIDW != RGET(b) % GRIDW)
-                printf("A = %d, B = %d\n", LGET(b), RGET(b));
+
+    //Solution 1
+//    unsigned char b = '\0';
+//    for(LSET(b, 1); LGET(b) <= GRIDW * GRIDW; LSET(b, (LGET(b) + 1)))
+//        for(RSET(b, 1); RGET(b) <= GRIDW * GRIDW; RSET(b, (RGET(b) + 1)))
+//            if(LGET(b) % GRIDW != RGET(b) % GRIDW)
+//                printf("A = %d, B = %d\n", LGET(b), RGET(b));
+//
+
+
+    //Solution 2
+    int i = 81;
+
+    while(i--)
+    {
+        if(i / 9 % 3 == i % 9 % 3)
+            continue;
+
+        printf("A = %d, B = %d\n", i / 9 + 1, i % 9 + 1);
+    }
+
+
+    //Solu
 
     return 0;
 }
