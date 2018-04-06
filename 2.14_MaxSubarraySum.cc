85c16a30fdfe67a4137efdf1365f9b034c384798
Mingkai Ma <mamingkai824@gmail.com>
2018-04-06 21:21:34 +1000

    max subarray sum

--- 2.14_MaxSubarraySum.cc
+++ 2.14_MaxSubarraySum.cc
@@ -2,35 +2,75 @@
 
 using namespace std;
 
-//solu1
+//if all is minus, return the maximum
 int MaxSum1(int* A, int n)
 {
-    int maxSum = INT_MIN;
+    int max_now = A[0];
+    int max_so_far = A[0];
+    for(int i = 1; i < n; i++)
+    {
+        max_now = max(A[i], max_now + A[i]);
+        max_so_far = max(max_so_far, max_now);
+    }
+    return max_so_far;
+}
+
+//if all is minus, return 0
+int MaxSum2(int* A, int n)
+{
+    int max_now = 0;
+    int max_so_far = 0;
+    for(int i = 0; i < n; i++)
+    {
+        max_now = max(0, max_now + A[i]);
+        max_so_far = max(max_now, max_so_far);
+    }
+
+    return max_so_far;
+}
+
+// return start index
+void MaxSum3(int* A, int n)
+{
     int sum = 0;
+    int maxSum = INT_MIN;
+    int startIndex = 0;
+    int maxIndex = 0;
 
     for(int i = 0; i < n; i++)
     {
-        sum = 0;
-        for(int j = i; j < n; j++)
+        if(sum + A[i] < A[i])
         {
-            sum += A[j];
-            if(sum > maxSum)
-                maxSum = sum;
+            cout << "iii: " << i << endl;
+            sum = A[i];
+            startIndex = i;
+        }
+        else
+            sum += A[i];
+
+
+        if(sum > maxSum)
+        {
+            maxSum = sum;
+            maxIndex = startIndex;
         }
     }
-    return maxSum;
+
+    int maxEndIndex = maxIndex;
+    int sumK = 0;
+    while(sumK != maxSum)
+    {
+        sumK += A[maxEndIndex];
+        maxEndIndex++;
+    }
+    maxEndIndex -= 1;
+    cout << "MaxSum is: " << maxSum << " start from " << maxIndex << " end in " << maxEndIndex << endl;
 }
 
-
-//solu2
-int MaxSum2(int* A, int n)
+int main()
 {
-    Start[n - 1] = A[n - 1];
-    All[n - 1] = A[n - 1];
-    for(int i = n - 2; i >= 0; i--)
-    {
-        Start[i] = max(A[i], A[i] + Start[i + 1]);
-        All[i] = max(Start[i], All[i + 1]);
-    }
-    return All[0];
+    //int A[] = {-2, 5, 3, -6, 4, -8, 6};
+    int A[] = {-4,-1,-2,-3};
+    //cout << MaxSum2(A, 7) << "\n";
+    MaxSum3(A, 4);
 }
